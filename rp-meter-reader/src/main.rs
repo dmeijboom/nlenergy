84e91402c75d2a85c63c8847eaad3f61c53e1300
Dillen Meijboom <work@dillen.dev>
2023-05-10 20:05:14 +0200

    feat: initial embedded project setup

---
+++ rp-meter-reader/src/main.rs
@@ -0,0 +1,17 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    loop {
+        defmt::info!("hello world");
+        Timer::after(Duration::from_secs(1)).await;
+    }
+}
